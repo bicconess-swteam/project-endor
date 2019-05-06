39d675daf4d5c64991d823e9d658f245b2947c60
Víctor (vjsantojaca) <victorsantoja@gmail.com>
2019-05-07 00:02:41 +0200

    Added EndorApp Application

---
+++ app/src/main/java/com/vjsantojaca/bicconess/swteam/projectendor/EndorApp.kt
@@ -0,0 +1,9 @@
+package com.vjsantojaca.bicconess.swteam.projectendor
+
+import android.app.Application
+
+class EndorApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
